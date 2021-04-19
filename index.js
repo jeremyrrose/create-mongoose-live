130c59fc6274d0cb61c28e57b98a91fec3b8976e
jeremyrrose <jeremy.r.rose@gmail.com>
2021-04-18 21:16:30 -0400

    0.0.5

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 #! /usr/bin/env node
+
+const main = async () => {
 console.log('adklajsdklsjgioefjlkafas')
+
+const { exec } = require('child-process')
+
+await exec("npm i mongoose-live")
+}
+
+main()
