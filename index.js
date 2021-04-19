8c260cc4caf5a2055efc3f86671a976fb0fc91e7
jeremyrrose <jeremy.r.rose@gmail.com>
2021-04-18 22:54:58 -0400

    inquirer syntax

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ const {prompt, checkbox} = require('inquirer')
 const main = async () => {
 
     console.log('Creating REPL for Mongoose project...')
-    await prompt({
+    const path = await prompt({
         type: "input",
-        name: "pathToModels",
+        name: "providedPath",
         message: "Please enter the path to your models directory:"
     })
-    await prompt({
+    const {proceed} = await prompt({
         type: "confirm",
         name: "proceed",
         message: "This will create repl.js, add a small node module, and add a script to package.json.\nProceed?",
         default: false
     })
-    console.log(prompt)
-    console.log(prompt.answers)
-    if (prompt.answers.proceed === true) {
+    console.log(path.providedPath)
+    console.log(proceed)
+    if (proceed === true) {
         await exec("npm i mongoose-live")
         addScript({key: "repl", value: "node --experimental-repl-await repl.js"})
     }
