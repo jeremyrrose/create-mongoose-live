c375d64896be89a0cc65799264c7eb49876a5428
jeremyrrose <jeremy.r.rose@gmail.com>
2021-04-18 23:05:39 -0400

    prepare to readdir

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 #! /usr/bin/env node
 
+const fs = require('fs')
 const { exec } = require('child_process')
 const addScript = require('npm-add-script')
-const {prompt, checkbox} = require('inquirer')
+const {prompt} = require('inquirer')
 
 const main = async () => {
 
     console.log('Creating REPL for Mongoose project...')
-    const path = await prompt({
+    const {path} = await prompt({
         type: "input",
-        name: "providedPath",
-        message: "Please enter the path to your models directory:"
+        name: "path",
+        message: "Please enter the path to your models directory:\nExample: ./models\n"
     })
+
+    const filenames = fs.readdir(path)
+    console.log(filenames)
+
+    const {models} = prompt({
+        type: "checkbox",
+        name: "models",
+        message: "Select models to include in REPL context:\(All available files selected by default.)",
+        loop: false
+    })
+
+
     const {proceed} = await prompt({
         type: "confirm",
         name: "proceed",
         message: "This will create repl.js, add a small node module, and add a script to package.json.\nProceed?",
         default: false
     })
-    console.log(path.providedPath)
-    console.log(proceed)
+
     if (proceed === true) {
         await exec("npm i mongoose-live")
         addScript({key: "repl", value: "node --experimental-repl-await repl.js"})
