ad2c2ee5dccdde3be381e48e457e53449359a273
jeremyrrose <jeremy.r.rose@gmail.com>
2021-04-18 22:26:42 -0400

    ajklasd

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 #! /usr/bin/env node
 
 const main = async () => {
-console.log('adklajsdklsjgioefjlkafas')
+    
+    console.log('adklajsdklsjgioefjlkafas')
 
-// const { exec } = require('child_process')
-const addScript = require('npm-add-script')
+    const { exec } = require('child_process')
+    const addScript = require('npm-add-script')
 
-await child_process.exec("npm i mongoose-live")
-addScript({key: "repl", value: "node --experimental-repl-await repl.js"})
-
+    await exec("npm i mongoose-live")
+    addScript({key: "repl", value: "node --experimental-repl-await repl.js"})
 
 }
 
